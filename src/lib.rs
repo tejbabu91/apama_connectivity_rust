60ae89cdfee9d5f28fe80270fdcc82f35c022f54
Gyan <github@gsbaiswar.com>
2020-04-01 00:05:54 +0530

    define new macro to expose required transport functions directly from rust (skipping C++)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,82 @@ pub mod api;
 
 pub use crate::api::*;
 
+use crate::api::ctypes as ctypes;
+use paste;
+
+// sag_is_host_shutting_down from c_functions.hpp was commented out because of using reference parameters
+// default parameter value was removed from sag_copy_custom in c_functions.hpp
+
+pub struct MyTestTransport {
+    data: i64
+}
+
+#[macro_export]
+macro_rules! DEFINE_RUST_TRANSPORT {
+    ($elem:ident) => {
+        paste::item! {
+            #[no_mangle]
+            pub extern fn [<sag_plugin_api_version_$elem>](p: ctypes::sag_plugin_t) -> ctypes::__uint64_t {
+                4
+            }
+
+            #[no_mangle]
+            pub extern fn [<sag_create_plugin_with_params_$elem>](name : *const ::std::os::raw::c_char, chainId: *const ::std::os::raw::c_char, config: ctypes::sag_underlying_data_t, _connectivityManager: *mut libc::c_void, _reserved: *mut libc::c_void) -> ctypes::sag_plugin_t {
+                println!("GYS: sag_create_plugin_with_params_MyNewTestTransport - start");
+                let t = Box::new(MyTestTransport{data: 42});
+
+                let p  = ctypes::sag_plugin_t { r#plugin: Box::into_raw(t) as *mut libc::c_void };
+
+                println!("GYS: sag_create_plugin_with_params_MyNewTestTransport - end");
+
+                p
+            }
+            #[no_mangle]
+            pub extern fn [<sag_destroy_plugin_$elem>](p: ctypes::sag_plugin_t) -> ctypes::sag_error_t {
+                println!("GYS: sag_destroy_plugin_MyNewTestTransport");
+
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+            #[no_mangle]
+            pub extern fn [<sag_plugin_start_$elem>](p: ctypes::sag_plugin_t) -> ctypes::sag_error_t {
+                unsafe {
+                    let pp = Box::from_raw(p.r#plugin as *mut MyTestTransport) ;
+                    println!("GYS: sag_plugin_start_MyNewTestTransport: {}", pp.data);
+                }
+
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+            #[no_mangle]
+            pub extern fn [<sag_plugin_shutdown_$elem>](p: ctypes::sag_plugin_t) -> ctypes::sag_error_t {
+                println!("GYS: sag_plugin_shutdown_MyNewTestTransport");
+
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+            #[no_mangle]
+            pub extern fn [<sag_plugin_hostReady_$elem>](p: ctypes::sag_plugin_t) -> ctypes::sag_error_t {
+                println!("GYS: sag_plugin_hostReady_MyNewTestTransport");
+
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+            #[no_mangle]
+            pub extern fn [<sag_plugin_setNextTowardsHost_$elem>](p: ctypes::sag_plugin_t, q: ctypes::sag_plugin_t, send_fn: ctypes::sag_set_next_fn_t)  -> ctypes::sag_error_t {
+                println!("GYS: sag_plugin_setNextTowardsHost_MyNewTestTransport");
+                // TODO - what do I need to implement here.
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+
+            #[no_mangle]
+            pub extern fn [<sag_plugin_sendBatchTowardsTransport_$elem>](p: ctypes::sag_plugin_t, start: *mut ctypes::sag_underlying_message_t, end: *mut ctypes::sag_underlying_message_t,) -> ctypes::sag_error_t {
+                println!("GYS: sag_plugin_sendBatchTowardsTransport_MyNewTestTransport");
+                // TODO - what do I need to implement here.
+                ctypes::sag_error_t_SAG_ERROR_OK
+            }
+        }
+    }
+}
+
+DEFINE_RUST_TRANSPORT!(MyTestTransport);
+
 #[macro_export]
 macro_rules! DefineTransport {
     ($elem:ident) => {
